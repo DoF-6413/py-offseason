cc98315621e657f0448ad24f42bccb8c4b016c53
Andres Lara <Andres.Lara@sisepuedefoundation.org>
2024-09-18 20:20:50 -0700

    add requirements

--- subsistemas/chassis/swerve.py
+++ subsistemas/chassis/swerve.py
@@ -11,12 +11,18 @@ modules = [4]
 gyro = subsistemas.gyro.Gyro
 maxAngularSpeed = 5 
 velocidadmaxima = 10
+timer = wpilib.Timer()
+
 class swerve():
 
   def __init__(self) -> None:
     #middle is at 1,1
+
     self.FLPose = wpimath.geometry.Translation2d(0,2)
     self.FRPose = wpimath.geometry.Translation2d(2.2)
     self.BLPose = wpimath.geometry.Translation2d(0,0)
     self.BRPose = wpimath.geometry.Translation2d(2,0)
+    timer.start
+  
+      
 
